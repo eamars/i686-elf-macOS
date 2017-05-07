cf2813b97ff2c15cb7c2181679c34f1f717e877b
Ran Bao <ran.bao@wirelessguard.co.nz>
2017-05-07 17:56:16 +1200

    Build gcc with newlib

---
+++ lib/gcc/i686-elf/7.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Users/eamars/Downloads/gcc-7.1.0/configure --disable-nls --target=i686-elf --disable-werror --prefix=/Users/eamars/Workspace/i686-elf --with-newlib --enable-languages=c,c++ --with-gmp=/usr/local/Cellar/gmp/6.1.2 --with-mpfr=/usr/local/Cellar/mpfr/3.1.5 --with-mpc=/usr/local/Cellar/libmpc/1.0.3_1 CC=gcc-7 CXX=g++-7 CPP=cpp-7 LD=gcc-7";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
